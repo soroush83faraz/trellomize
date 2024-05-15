9d0a02e3eae9602fb086244d06f44291a9b42a9c
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-15 12:27:42 +0330

    manager.py

--- manager.py
+++ manager.py
@@ -13,9 +13,20 @@ def main () :
     # print(f"username : {args.username} and {args.function_name}")
     if args.function_name[0] == "create-admin" :
         create_admin(args.username[0] , args.password[0])
-    # if args.function_name[0] == "purge-data" :
-    #     create_admin(args.username[0] , args.password[0])
-    
+    if args.function_name[0] == "purge-data" :
+        check = ""
+        print("you are deleting everything , are you sure ?")
+        while check != "NO" and check != "YES" :
+
+            check = input("if you are sure type (YES) and if you are not type (NO)")
+            if check == "yes" :
+                print("you should type it in capital letter.")
+                check = input("if you are sure type (YES) and if you are not type (NO)")
+            elif check == "YES" :
+                purge_data()
+            elif check == "NO" :
+                print()
+
         
 
 def create_admin(username , password) :
@@ -25,10 +36,12 @@ def create_admin(username , password) :
         json.dump(data, json_file)
         print(data)
 
+def purge_data () :
+    data = ''
 
-
-
-
+    with open('admin.json' , 'w') as json_file:
+        json.dump(data, json_file)
+        print("no more data")
 
 
 if __name__ == "__main__":
