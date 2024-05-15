3a06b6c28827d3524f4c0af824496619597d6131
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-15 16:39:28 +0330

    color

--- manager.py
+++ manager.py
@@ -1,8 +1,10 @@
 import json
-from rich import print as rprint
+from rich.console import Console
+from rich.text import Text
 import os
 import argparse
 
+con = Console()
 parser = argparse.ArgumentParser()
 
 def main () :
@@ -17,17 +19,24 @@ def main () :
         create_admin(args.username[0] , args.password[0])
     if args.function_name[0] == "purge-data" :
         check = ""
-        print("you are deleting everything , are you sure ?")
+        con.print("you are deleting everything , are you [underline]sure[/underline] ?" , style="bold")
         while check != "NO" and check != "YES" :
-
-            check = input("if you are sure type (YES) and if you are not type (NO)")
+            con.print("\nif you are sure type ([bold green]YES[/bold green]) and if you are not type ([bold red]NO[/bold red]) :")
+            check = input()
+            clear()
             if check == "yes" :
-                print("you should type it in capital letter.")
-                check = input("if you are sure type (YES) and if you are not type (NO)")
-            elif check == "YES" :
+                clear()
+                con.print("you should type it in [underline]capital[/underline] letter.\n")   
+                con.print("\nif you are sure type ([bold green]YES[/bold green]) and if you are not type ([bold red]NO[/bold red]) :")
+                check = input()
+            if check == "YES" :
                 purge_data()
-            elif check == "NO" :
+            if check == "NO" :
                 print()
+            if check != "YES" and check != "NO" :
+                con.print("your choice is not valid type [bold green]YES[/bold green] or [bold red]NO[/bold red]")   
+
+                
 
 def create_admin(username , password) :
     data = {"username" : username , "password": password}
@@ -43,16 +52,17 @@ def create_admin(username , password) :
     if input_from_json == None :    
         with open('admin.json' , 'w') as json_file:
             json.dump(data, json_file)
-            print(data)
+            con.print(data)
     else :
-        print("you dont have the acces to make an admin")
+        clear()
+        con.print("you dont have the [bold yellow]acces[/bold yellow] to make an admin")
             
             
         
 def purge_data () :
     with open('admin.json' , 'w') as json_file:
         json.dump(None, json_file)
-        print("no more data")
+        con.print("no more data" , style="blue")
                
 def clear():
     os.system('cls')
