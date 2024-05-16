ecb2b0b7332a5d86131ce461c19a2fd93346867b
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-16 03:36:21 +0330

    tasks

--- tasks.py
+++ tasks.py
@@ -1,7 +1,30 @@
 import datetime
+import uuid
+from enum import Enum
+from datetime import timedelta
+
+
+
+#Enum class for Task_Status ========================================
+class Task_Status (Enum) :
+    BACKLOG = 0
+    TODO  = 1
+    DOING = 2
+    DONE = 3
+    ARCHIVED = 4
+
+
+#Enum class for prioritys ========================================
+class Task_priority(Enum) :
+    CRITICAL = 0
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
 #Function to making unique ID for our task using uuID library======
 def make_unique_ID():
-    pass
+    return uuid.uuid4()
+
 
 
 #==================================================================
@@ -18,8 +41,13 @@ class Task:
     history = None
     comments  = []
 
-    def __init__(self , title):
+    def __init__(self , priority = Task_priority.LOW , title = "Task" , discription = "put your discription here" , status = Task_Status.BACKLOG ) :
+        
+        self.status = status
+        self.discription = discription
+        self.priority = priority
         self.title = title
-        self.start_time = (datetime.datetime.now()).strftime("%Y-%m-%d   %H")
-        self.end_time = self.start_time + datetime.timedelta(hours=24)
+        self.start_time = (datetime.datetime.now()).strftime("%Y-%m-%d  %H:%M:%S")
+        self.end_time = (datetime.datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d  %H:%M:%S")
         self.ID  = make_unique_ID()
+        
