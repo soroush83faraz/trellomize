95c5adf9d7a7a431db64a9f394b8e65e2530fce5
SaeedGeshani <154725043+SaeedGeshani@users.noreply.github.com>
2024-05-17 23:29:17 +0330

    Moving with thumb is done

--- test.py
+++ test.py
@@ -1,4 +1,34 @@
-a = 7 
-b = 4
-c = a//2/ + b**2
-print(a//2)
+# import json
+
+
+# try :
+#     with open("save_username_password_email.json" , "r") as json_file :
+#         users_info = json.load(json_file)
+#         json_file.close()
+            
+            
+# except FileNotFoundError:
+#    users_info = []
+
+# print(users_info)
+
+ls1 = [1,2,3,4]
+ls2 = [11 , 22 , 33 , 44]
+ls3 = [111 , 222 , 333 ,444]
+ls4 = [1111 , 2222 , 3333 , 4444]
+
+sRR = [[0, 0 , 0 , 0] for _ in range(4)]
+
+
+for i in range(len(ls1)):
+    sRR[i][0] = ls1[i]
+for i in range(len(ls2)):
+    sRR[i][1] = ls2[i]
+for i in range(len(ls3)):
+    sRR[i][2] = ls3[i]
+for i in range(len(ls4)):
+    sRR[i][3] = ls4[i]
+
+
+
+print(sRR)
