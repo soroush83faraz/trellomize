71a82e3879b18b7dfe9be562c7a129a5808eb866
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-24 23:02:24 +0330

    global

--- test_global_functios.py
+++ test_global_functios.py
@@ -1,11 +1,18 @@
 from comment_and_member import Add_Comment
 from comment_and_member import assigning_task_to_member
 from in_project_workplace import *
+from making_new_account import *
 import unittest
 import json
 
 
 class TestAddComment(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        print("\nADD_COMMENT ==============")
+       
+
     def test_add_comment(self):
 
         ID = "c3beb888-12f8-4589-8421-6df7a01fce24"
@@ -32,6 +39,11 @@ class TestAddComment(unittest.TestCase):
         self.assertTrue(found_comment, f"Comment didnt wrote for task ID: {ID}")
 
 class TestAssigningTaskToMember(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        print("\nASSIGNING_TASK_TO_MEMBER ==============")
+    
     def test_assign_task_to_member(self):
         
         ID = "c3beb888-12f8-4589-8421-6df7a01fce24"
@@ -56,13 +68,51 @@ class TestAssigningTaskToMember(unittest.TestCase):
             self.assertTrue(found_task, f"Task has been assigned for this member")
 
 # class Testshow_task(unittest.TestCase) :
+
+#     @classmethod
+#     def setUpClass(cls):
+#         print("\nSHOW_TASK ==============")
+    
 #     def test_show_task(self) :
 #         proj_path_leads = [0, 'projects_leads', 'tasks']
 #         return_value = show_task(proj_path_leads)
         
 #         self.assertEqual(return_value[0] ,{'Title': 'MY_F', 'Description': 'Just for testing this section of my code', 'Priority': 'CRITICAL', 'Status': 'DOING', 'Comments': [], 'ID': '8359ddfd-1b73-41fd-a2d2-557d46c0ee4a'})
 
-class 
+class Testfinding_projects_leads(unittest.TestCase) :
+    
+    @classmethod
+    def setUpClass(cls):
+        print("\nFINDING_PROJECTS_LEADS ==============")
+    
+    def test_finding_projects_leads(self) :
+        ID  = "1323"
+        username = "saeed"
+        return_value = finding_projects_leads(ID , username)
+        self.assertEqual(return_value , [0, 'projects_leads', 'tasks'])
+
+class Testfinding_projects_member(unittest.TestCase) :
+    
+    @classmethod
+    def setUpClass(cls):
+        print("\nFINDING_PROJECTS_MEMBER ==============")
+    
+    def test_finding_projects_member(self) :
+        ID  = "1325"
+        username = "soroush"
+        return_value = finding_projects_member(ID , username)
+        self.assertEqual(return_value , [2, 'projects_member', 'tasks'])
+
+# class Testmy_validate_email(unittest.TestCase) :
+
+#     @classmethod
+#     def setUpClass(cls):
+#         print("\nVALIDATE_EMAIL ==============")
+    
+#     def test_my_validate_email (self) :
+#         default_email = "s.s@gmail.com"
+#         my_emailaddress =  my_validate_email()
+#         self.assertEqual(my_emailaddress , default_email)
 
 
 
