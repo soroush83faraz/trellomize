01147a669e62bdf8a7bdafe6a89e5a456005070f
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-24 22:12:34 +0330

    working with unittest

--- test_global_functios.py
+++ test_global_functios.py
@@ -1,5 +1,6 @@
 from comment_and_member import Add_Comment
 from comment_and_member import assigning_task_to_member
+from in_project_workplace import *
 import unittest
 import json
 
@@ -32,8 +33,8 @@ class TestAddComment(unittest.TestCase):
 
 class TestAssigningTaskToMember(unittest.TestCase):
     def test_assign_task_to_member(self):
+        
         ID = "c3beb888-12f8-4589-8421-6df7a01fce24"
-
         assigning_task_to_member(ID)
 
         with open("save_username_password_email.json", "r") as json_file:
@@ -53,6 +54,17 @@ class TestAssigningTaskToMember(unittest.TestCase):
         if found_task :
             print(f"Task has been assigned for this member")
             self.assertTrue(found_task, f"Task has been assigned for this member")
+
+# class Testshow_task(unittest.TestCase) :
+#     def test_show_task(self) :
+#         proj_path_leads = [0, 'projects_leads', 'tasks']
+#         return_value = show_task(proj_path_leads)
+        
+#         self.assertEqual(return_value[0] ,{'Title': 'MY_F', 'Description': 'Just for testing this section of my code', 'Priority': 'CRITICAL', 'Status': 'DOING', 'Comments': [], 'ID': '8359ddfd-1b73-41fd-a2d2-557d46c0ee4a'})
+
+class 
+
+
 
 
 
