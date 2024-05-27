970d57710972322cd124b650d9123e897da2685e
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2024-05-27 17:25:01 +0330

    manager

--- manager.py
+++ manager.py
@@ -11,10 +11,28 @@ def main () :
     parser.add_argument("function_name" , nargs=1 , type=str , help="calls the function name if there is one" )
     parser.add_argument("--password" , nargs=1 , type=str , help="gets password" )
     parser.add_argument("--username" , nargs=1 , type=str , help="gets username" )
+    # parser.add_argument("--changefield" , nargs=1 , type=str , help="get field")
     args = parser.parse_args()
     clear()
 
     # print(f"username : {args.username} and {args.function_name}")
+    if args.function_name[0] == "changefield" :
+        while True :
+            show_is_active()
+            username = input("username : ")
+            try :
+                with open('save_username_password_email.json' , 'r') as json_file :
+                    input_from_json = json.load(json_file)     
+            except :
+                input_from_json = []
+                
+            for user in input_from_json :
+                if username == user["username"] :
+                    changing_is_active(username)
+                    return
+                 
+        
+
     if args.function_name[0] == "create-admin" :
         create_admin(args.username[0] , args.password[0])
     if args.function_name[0] == "purge-data" :
@@ -66,6 +84,41 @@ def purge_data () :
     with open('save_username_password_email.json' , 'w') as json_file:
         json.dump(None, json_file)
               
+def show_is_active () :
+    try :
+        with open ("save_username_password_email.json" , "r") as json_file :
+            user_info = json.load(json_file)
+            json_file.close()
+                
+    except :
+        user_info = []
+        
+    for user in user_info :
+        print(user["username"] , "IsActive =" , user["IsActive"] )    
+        
+def changing_is_active (username) :
+    try :
+        with open ("save_username_password_email.json" , "r") as json_file :
+            user_info = json.load(json_file)
+            json_file.close()
+                
+    except :
+        user_info = []
+        
+    for user in user_info :
+        if user["username"] == username :
+            if user["IsActive"] == True :
+                user["IsActive"] = False
+                print(f"{username} Is Deactive")
+            else :
+                user["IsActive"] = True
+                print(f"{username} Is Active")
+            with open('save_username_password_email.json' , 'w') as json_file:
+                json.dump(user_info, json_file)               
+        
+    
+    
+    
         
                
 def clear():
